436f9d0531d7f8f0b9dfbad97fb3b58f13455422
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-07-18 18:34:27 +0200

    Implement the Page Not Found

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+
+import "./style.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import NotFoundSvg from "../../assets/not-found.svg";
 
 const NotFoundPage = () => {
-    return (
-        <div>
-            <h1>NOT FOUND PAGE</h1>
-        </div>
-    )
-}
+  return (
+    <div className="page-not-found">
+      <Header subHeading="Page Not Found" />
+      <div className="svg-container">
+        <img src={NotFoundSvg} alt="Not Found" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default NotFoundPage;
